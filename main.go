80f33ef729129ecc5f441acea248e91c6c8d21e6
CMachine <Jcham55@gmail.com>
2020-08-16 22:11:21 -0400

    testing struct

--- main.go
+++ main.go
@@ -1,18 +1,56 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zipCode int
+
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contact contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
 func main() {
-	cards := newDeck()
-	cards.print()
 
-	// deal func
-	hand, remainingCards := deal(cards, 5)
-	// print
-	hand.print()
-	remainingCards.print()
+	jim := person{
+		firstName: "Jim",
+		lastName: "Anderson",
+		contact: contactInfo{
+			email: "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jim.updateName("new_name")
+	jim.print()
 
-	// save cards to file
-	cards.saveToFile("my_cards.csv")
 
-	// shuffle
-	remainingCards.shuffle()
+	//cards := newDeck()
+	//cards.print()
+	//
+	//// deal func
+	//hand, remainingCards := deal(cards, 5)
+	//// print
+	//hand.print()
+	//remainingCards.print()
+	//
+	//// save cards to file
+	//cards.saveToFile("my_cards.csv")
+	//
+	//// shuffle
+	//remainingCards.shuffle()
+
+	// struct
+
 }
